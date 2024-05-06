ec74589b228c6c4722e401b85d964c9962be4394
armenta lindoro maria lourdes <137097961+mariaLourdesArmenta@users.noreply.github.com>
2024-05-06 14:47:13 +0000

    Actualizacion Recorridos

---
+++ arbolBinario/ArbolBinario.java
@@ -0,0 +1,71 @@
+package arbolBinario;
+import pilas.Pila;
+
+public class ArbolBinario{
+    private Nodo raiz;
+    public ArbolBinario(){
+        this.raiz = null;
+    }
+    public ArbolBinario(Nodo raiz){
+        this.raiz = raiz;
+    
+    }
+    public Nodo getRaiz(){
+        return this.raiz;
+    }
+    public void setRaiz(Nodo raiz){
+        this.raiz = raiz;
+    }
+    
+    private void visitar(Nodo aux){
+        System.out.print(aux.getValor()+" ");
+    }
+    /*
+     * Método recursivo en pre-orden
+     */
+    private void preorden(Nodo aux){
+        if(aux !=null){
+            visitar(aux);
+            preorden(aux.getIzquierdo());
+            preorden(aux.getDerecho());
+
+        }
+    }
+    public void preorden(){
+        preorden(raiz); //se invoca al método recursivo
+    }
+
+    //Método recursivo inOrden
+    private void inorden(Nodo aux){
+        if(aux !=null){
+            inorden(aux.getIzquierdo());
+            visitar(aux);
+            inorden(aux.getDerecho());
+        }
+    }
+
+    public void inorden(){
+        inorden(raiz);
+    }
+
+    //Método recursivo postOrden
+    private void postorden(Nodo aux){
+        if(aux !=null){
+            postorden(aux.getIzquierdo());
+            postorden(aux.getDerecho());
+            visitar(aux);
+        }
+    }
+    public void postorden(){
+        postorden(raiz);
+    }
+
+    //Recorrido Iterativo en Preorden, utilizando una pila
+    public void preordenIterativo(){
+        Pila<Nodo> pila = new Pila<Nodo>();
+    }
+
+    
+
+    
+}
