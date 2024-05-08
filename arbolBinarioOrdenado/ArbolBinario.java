a13203c11600633837214d9ccf71e9faf65ebfd3
armenta lindoro maria lourdes <137097961+mariaLourdesArmenta@users.noreply.github.com>
2024-05-08 16:01:06 +0000

    Actualizacion Arboles binarios ordenados insertar y buscar

--- arbolBinarioOrdenado/ArbolBinario.java
+++ arbolBinarioOrdenado/ArbolBinario.java
@@ -118,6 +118,113 @@ public class ArbolBinario{
             aux = aux.getDerecho();
         }
     }
+    //insertar un nodo en el árbol versión iterativa
+    public void insertar(Object valor) throws Exception{
+        Comparable dato = (Comparable) valor;
+        Nodo nuevo = new Nodo();
+        nuevo.setValor(dato);
+
+        if (raiz==null)
+           raiz = nuevo;
+        else{
+            //anterior es una referencia al padre de aux
+            Nodo anterior = null;
+            //aux es un auxiliar que va recorriendo los nodos, desde la raiz
+            Nodo aux = raiz;
+            while (aux != null){
+                anterior = aux;
+                if (dato.esMenor(aux.getValor()))
+                   aux = aux.getIzquierdo();
+                else if (dato.esMayor(aux.getValor()))
+                   aux = aux.getDerecho();
+                else 
+                   throw new Exception("Dato duplicado");
+
+            }
+            if (dato.esMenor(anterior.getValor()))
+               anterior.setIzquierdo(nuevo);
+            else 
+               anterior.setDerecho(nuevo);
+        }
+
+    }
+    
+    //Insertar, versión recursiva
+    public void insertar2(Object valor) throws Exception{
+        Comparable dato = (Comparable) valor;
+        raiz = insertarRec(raiz,dato);
+    }
+
+    private Nodo insertarRec(Nodo raizSub, Comparable dato) throws Exception{
+        if (raizSub == null){
+            //caso base, termina la recursividad
+            raizSub = new Nodo(dato);
+        }
+        else{
+            if (dato.esMenor(raizSub.getValor())){
+                Nodo iz = insertarRec(raizSub.getIzquierdo(),dato);
+                raizSub.setIzquierdo(iz);   
+            }
+            else{
+                if (dato.esMayor(raizSub.getValor())){
+                    Nodo dr = insertarRec(raizSub.getDerecho(),dato);
+                    raizSub.setDerecho(dr);   
+                }
+                else{
+                    //dato duplicado
+                    throw new Exception("Nodo Duplicado");
+                }
+            }
+        }
+        return raizSub;
+
+    }
+
+    //Busca un dato en el árbol comenzando desde la raiz, versión iterativa
+    /*
+     * Si lo encuentra, retorna un Nodo en caso contrario regresa null
+     */
+    public Nodo buscar(Object valor){
+        Comparable dato = (Comparable) valor;
+        if (raiz==null)
+          return raiz;
+        else{
+            //aux, es un auxiliar que va recorriendo los nodos, desde la raiz
+            Nodo aux = raiz;
+            while( aux !=null){
+                if (dato.esIgual(aux.getValor()))
+                   return aux;
+                if(dato.esMenor(aux.getValor()))
+                   aux = aux.getIzquierdo();
+                else  
+                   aux = aux.getDerecho();
+
+            }
+            return null;
+        }
+
+    }
+    //Version Recursiva
+    public Nodo buscar2(Object buscado){
+        Comparable dato = (Comparable) buscado;
+        if (raiz ==null)
+           return null;
+        else 
+           return localizar(raiz,dato);
+    }
+
+    public Nodo localizar(Nodo raizSub, Comparable buscado){
+        if (raizSub == null)
+           return null;
+        else if(buscado.esIgual(raizSub.getValor()))
+           return raizSub;
+        else if(buscado.esMenor(raizSub.getValor()))
+           return localizar(raizSub.getIzquierdo(),buscado);
+        else
+           return localizar(raizSub.getDerecho(),buscado);
+           
+    }
+
 
     
 
